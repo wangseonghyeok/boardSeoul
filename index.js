40f16f3fd80222520413788ae89eab8dd855e8ef
wang seong hyeok <wang7807@naver.com>
2022-09-14 19:56:44 +0900

    팝업jax 호출

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const dotenv = require("dotenv").config();
 const cloudinary = require("cloudinary");
 const multer = require("multer");
 const { nextTick } = require("process");
+
+//데이터 서버에 저장
 app.use(
   session({
     secret: "codenumber",
@@ -16,6 +18,8 @@ app.use(
     cookie: { maxAge: 1000 * 60 * 60, secure: false },
   })
 );
+
+//인증요청처리
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(
@@ -49,9 +53,11 @@ passport.use(
   )
 );
 
+//사용자 정보 객체를 세션에 아이디로 저장
 passport.serializeUser((user, done) => {
   done(null, user.userID);
 });
+//세션에 저장한 아이디를 통해서 사용자 정보 객체를 불러옴
 passport.deserializeUser((id, done) => {
   db.collection("member").findOne({ userID: id }, (err, result) => {
     done(null, result);
@@ -61,6 +67,7 @@ passport.deserializeUser((id, done) => {
   });
 });
 
+//mongodb connect
 const MongoClient = require("mongodb").MongoClient;
 let db = null;
 MongoClient.connect(process.env.MONGO_URL, { useUnifiedTopology: true }, (err, client) => {
@@ -73,24 +80,38 @@ MongoClient.connect(process.env.MONGO_URL, { useUnifiedTopology: true }, (err, c
 app.set("port", process.env.PORT || 8099);
 const PORT = app.get("port");
 app.set("view engine", "ejs");
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use("/upload", express.static(path.join(__dirname, "/upload")));
 
+//cloudinary db
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+//이미지를 서버 디스크에 저장
 const storage = multer.diskStorage({});
-
 const fileUpload01 = multer({ storage: storage });
 const fileUpload02 = multer({ storage: storage });
-
+//로그인
 app.get("/login", (req, res) => {
   res.render("index", { title: "로그인", userInfo: req.user });
 });
+app.post("/login", passport.authenticate("local", { failureRedirect: "/login", successRedirect: "/main" }));
+
+app.get("/logout", (req, res) => {
+  if (req.user) {
+    req.session.destroy(function (err) {
+      res.send(`<script>alert("로그아웃되었습니다."); location.href="/login"</script>`);
+    });
+  } else {
+    res.send(`<script>alert("서버가 끊겼습니다."); location.href="/login"</script>`);
+  }
+});
+//회원가입
 app.get("/register", (req, res) => {
   res.send(`아이디는 ${req.query.userID}==패스워드는 ${req.query.userPW}`);
 });
@@ -107,105 +128,6 @@ app.post("/registerAjax", (req, res) => {
     res.json({ isJoin: true });
   });
 });
-app.get("/logout", (req, res) => {
-  if (req.user) {
-    req.session.destroy(function (err) {
-      res.send(`<script>alert("로그아웃되었습니다."); location.href="/login"</script>`);
-    });
-  } else {
-    res.send(`<script>alert("서버가 끊겼습니다."); location.href="/login"</script>`);
-  }
-});
-app.get("/main", (req, res) => {
-  if (req.user) {
-    db.collection("list")
-      .find()
-      .toArray((err, result) => {
-        res.render("main", { title: "Seoul Photography List", main: result });
-      });
-  } else {
-    res.send(`<script>alert("서버가 끊겼습니다."); location.href="/login"</script>`);
-  }
-});
-
-app.get("/mypage", (req, res) => {
-  // console.log(req.user.userID);
-  if (req.user) {
-    res.render("mypage", { title: "My Page", userInfo: req.user });
-  } else {
-    res.send(`<script>alert("서버가 끊겼습니다."); location.href="/login"</script>`);
-  }
-});
-app.post("/mypage", (req, res) => {
-  const userID = req.body.userID;
-  const userPW = req.body.userPW;
-  const userName = req.body.userName;
-
-  db.collection("member").updateOne({ userID: userID }, { $set: { userID: userID, userPW: userPW, userName: userName } }, (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(result);
-    res.send(`<script>alert("회원정보 수정이 되었습니다.");location.href="/login";</script>`);
-  });
-});
-app.get("/detail/:title", (req, res) => {
-  const title = req.params.title;
-  db.collection("list").findOne({ title: title }, (err, result) => {
-    // if (result) {
-    //   res.render("detail", { title: "detail", data: result });
-    // }
-    if (err) {
-      console.log(err);
-    }
-  });
-});
-app.post("/detail:edit", (req, res) => {
-  const title = req.body.title;
-  const date = req.body.date;
-  const desc = req.body.desc;
-  const point = req.body.point;
-  const insert = {
-    title: title,
-    date: date,
-    desc: desc,
-    point: point,
-  };
-  console.log(req);
-  db.collection("list").updateOne(insert);
-  console.log(insert);
-
-  // db.collection("list").updateOne({ $set: { title: title, date: date, desc: desc, point: point } }, (err, result) => {
-  //   if (err) {
-  //     console.log(err);
-  //   }
-  //   console.log(result);
-  //   res.send(`<script>alert("회원정보 수정이 되었습니다.");location.href="/login";</script>`);
-  // });
-});
-
-app.post("/login", passport.authenticate("local", { failureRedirect: "/login", successRedirect: "/main" }));
-
-app.get("/delete", (req, res) => {
-  res.render("delete", { title: "Member Delete" });
-});
-app.post("/delete", (req, res) => {
-  if (req.user) {
-    console.log(req.user.userID);
-    const userPW = req.body.userPW;
-    db.collection("member").deleteOne({ userID: req.user.userID, userPW: userPW }, (err, result) => {
-      console.log(result);
-      if (result.deletedCount > 0) {
-        res.send(`<script>alert("회원탈퇴 되었습니다.");location.href="/login"</script>`);
-      } else {
-        res.send(`<script>alert("비밀번호 확인해주세요.");location.href="/delete";</script>`);
-      }
-    });
-  } else {
-    res.send(`<script>alert("서버가 끊겼습니다."); location.href="/login"</script>`);
-  }
-});
-
 app.post("/register", (req, res) => {
   const userID = req.body.userID;
   const userPW = req.body.userPW;
@@ -227,6 +149,110 @@ app.post("/register", (req, res) => {
     res.send(`<script>alert("회원가입이 잘 되었습니다.");location.href="/"</script>`);
   });
 });
+//컨텐츠
+app.get("/main", (req, res) => {
+  if (req.user) {
+    db.collection("list")
+      .find()
+      .toArray((err, result) => {
+        res.render("main", { title: "Seoul Photography List", main: result });
+        //res.json(result);
+      });
+  } else {
+    res.send(`<script>alert("서버가 끊겼습니다."); location.href="/login"</script>`);
+  }
+});
+app.get("/mypage", (req, res) => {
+  // console.log(req.user.userID);
+  if (req.user) {
+    res.render("mypage", { title: "My Page", userInfo: req.user });
+  } else {
+    res.send(`<script>alert("서버가 끊겼습니다."); location.href="/login"</script>`);
+  }
+});
+
+app.post("/mypage", (req, res) => {
+  const userID = req.body.userID;
+  const userPW = req.body.userPW;
+  const userName = req.body.userName;
+
+  db.collection("member").updateOne({ userID: userID }, { $set: { userID: userID, userPW: userPW, userName: userName } }, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+    res.send(`<script>alert("회원정보 수정이 되었습니다.");location.href="/login";</script>`);
+  });
+});
+// app.get("/detail/:title", (req, res) => {
+//   const title = req.params.title;
+//   db.collection("list").findOne({ title: title }, (err, result) => {
+//     // if (result) {
+//     //   res.render("detail", { title: "detail", data: result });
+//     // }
+//     if (err) {
+//       console.log(err);
+//     }
+//   });
+// });s
+app.post("/detail/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id);
+  db.collection("list").findOne({ no: id }, (err, result) => {
+    if (result) {
+      res.json(result);
+    }
+    if (err) {
+      console.log(err);
+    }
+  });
+});
+
+app.post("/edit", fileUpload01.single("image"), (req, res) => {
+  const title = req.body.title;
+  const date = req.body.date;
+  const desc = req.body.desc;
+  const point = req.body.point;
+  const no = req.body.no;
+  // const image = req.file.filename;
+  // const total = result.totalPost;
+  const insert = {
+    title: title,
+    date: date,
+    desc: desc,
+    point: point,
+  };
+
+  console.log(insert);
+  db.collection("list").updateOne({ no: no }, { $set: { title: title, date: date, desc: desc, point: point } }, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(result);
+      res.json({ isOk: "ok", editData: { title, date, desc, point } });
+    }
+  });
+});
+app.get("/delete", (req, res) => {
+  res.render("delete", { title: "Member Delete" });
+});
+app.post("/delete", (req, res) => {
+  if (req.user) {
+    console.log(req.user.userID);
+    const userPW = req.body.userPW;
+    db.collection("member").deleteOne({ userID: req.user.userID, userPW: userPW }, (err, result) => {
+      console.log(result);
+      if (result.deletedCount > 0) {
+        res.send(`<script>alert("회원탈퇴 되었습니다.");location.href="/login"</script>`);
+      } else {
+        res.send(`<script>alert("비밀번호 확인해주세요.");location.href="/delete";</script>`);
+      }
+    });
+  } else {
+    res.send(`<script>alert("서버가 끊겼습니다."); location.href="/login"</script>`);
+  }
+});
+
 app.post("/library", fileUpload01.single("image"), (req, res) => {
   db.collection("counter").findOne({ name: "total" }, (err, result) => {
     const title = req.body.title;
@@ -288,17 +314,18 @@ app.get("/write", (req, res) => {
     res.send(`<script>alert("서버가 끊겼습니다."); location.href="/login"</script>`);
   }
 });
-app.post("/idCheck", (req, res) => {
-  const userID = req.body.userID;
-  db.collection("member").findOne({ userID: userID }, (err, result) => {
-    //console.log(result);
-    if (result === null) {
-      res.json({ isOk: true });
-    } else {
-      res.json({ isOk: false });
-    }
-  });
-});
+
+// app.post("/idCheck", (req, res) => {
+//   const userID = req.body.userID;
+//   db.collection("member").findOne({ userID: userID }, (err, result) => {
+//     //console.log(result);
+//     if (result === null) {
+//       res.json({ isOk: true });
+//     } else {
+//       res.json({ isOk: false });
+//     }
+//   });
+// });
 app.listen(PORT, () => {
   console.log(`${PORT}에서 서버 대기중`);
 });
